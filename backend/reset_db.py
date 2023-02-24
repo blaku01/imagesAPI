d0bb2b8165820a4743c1669b6b3ad7ba17866e0f
blaku01 <55555541+blaku01@users.noreply.github.com>
2023-02-24 22:47:01 +0100

    created reset_db.py
    created a script, which wipes out database, makes migrations and sets up default AccountTiers and superuser

---
+++ backend/reset_db.py
@@ -0,0 +1,47 @@
+import os
+
+# set up Django environment
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
+from users.factories import AccountTierFactory, ImagesUserFactory
+
+from django.core.management import call_command
+
+# call Django management commands to reset database and run migrations
+call_command("reset_db", "--noinput", "--close-sessions")
+call_command("migrate")
+
+# create account tiers
+
+basic_tier = AccountTierFactory(
+    name="Basic",
+    thumbnail_sizes=[200],
+    original_file_link=False,
+    expiring_link_enabled=False,
+)
+
+premium_tier = AccountTierFactory(
+    name="Premium",
+    thumbnail_sizes=[200, 400],
+    original_file_link=True,
+    expiring_link_enabled=False,
+)
+
+enterprise_tier = AccountTierFactory(
+    name="Enterprise",
+    thumbnail_sizes=[200, 400],
+    original_file_link=True,
+    expiring_link_enabled=True,
+)
+
+# create users
+
+staff_user = ImagesUserFactory(
+    email="admin@admin.com",
+    account_tier=enterprise_tier,
+    is_staff=True,
+    is_superuser=True,
+    password="admin",
+)
