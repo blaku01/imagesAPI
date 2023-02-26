fd372eb40b25e3605715da23fccbb3dab8164da4
blaku01 <gabrielblaczek@gmail.com>
2023-02-26 08:58:56 +0100

    moved user factories to tests, added username sequence field to ImagesUserFactory

--- backend/users/tests/test_models.py
+++ backend/users/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.utils import timezone
-from users.factories import AccountTierFactory
+from users.tests.factories import AccountTierFactory
 
 User = get_user_model()
 
