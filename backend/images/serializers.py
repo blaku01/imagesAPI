be586f733b93aa668bb5394fe450c722b663d714
blaku01 <gabrielblaczek@gmail.com>
2023-02-26 02:57:39 +0100

    formatted images/serializers.py

--- backend/images/serializers.py
+++ backend/images/serializers.py
@@ -1,12 +1,17 @@
-from django.urls import reverse
 from rest_framework import serializers
+
 from .models import Image
+
+
 class ListImageSerializer(serializers.HyperlinkedModelSerializer):
-    url = serializers.HyperlinkedIdentityField(view_name="images-detail", read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+        view_name="images-detail", read_only=True
+    )
+
     class Meta:
         model = Image
-        fields = ('id', 'name', 'url', 'created_at')
-        read_only_fields = ('id', 'name', 'created_at')
+        fields = ("id", "name", "url", "created_at")
+        read_only_fields = ("id", "name", "created_at")
 
 
 class DetailImageSerializer(serializers.Serializer):
@@ -14,8 +19,9 @@ class DetailImageSerializer(serializers.Serializer):
     original_image = serializers.URLField(required=False)
     expiring_binary_url = serializers.URLField(required=False)
 
+
 class CreateImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
-        fields = ('id', 'name', 'file', 'created_at')
-        read_only_fields = ('id', 'created_at')
+        fields = ("id", "name", "file", "created_at")
+        read_only_fields = ("id", "created_at")
